2cb8c9cc5993b1d3a3d67cb142d51ce6be3e44c7
cowardclaire <cowardclaire@hotmail.co.uk>
2026-08-09 16:30:40 +0100

    linking cleaned data into my visualisations .py file

--- src/visualisations.py
+++ src/visualisations.py
@@ -1,2 +1,25 @@
 #Purpose : This file contains reusable Plotly, Matplotlib, or Seaborn chart functions, such as: time-series charts, bar charts,
 #scatter plots, box plots, customer segmentation visuals
+
+import matplotlib.pyplot as plt
+import seaborn as sns   
+import pandas as pd
+import numpy as np
+import os
+
+# Build path to cleaned data
+cleaned_path = os.path.join(
+    os.path.dirname(__file__),   # folder where visualisations.py lives (src)
+    "..",                        # go up to CAPSTONE-PROJECT
+    "data",                      # enter data folder
+    "cleaned-data",              # enter cleaned-data folder
+    "cleaned_data.csv"           # your cleaned file
+)
+
+# Load cleaned dataset
+df = pd.read_csv(cleaned_path)
+
+print("Loaded cleaned data successfully")
+print(df.head())
+
+
