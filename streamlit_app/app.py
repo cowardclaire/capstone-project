38488212ff2643d0f24c205c45f59b14e8caff60
cowardclaire <cowardclaire@hotmail.co.uk>
2026-09-03 09:59:28 +0100

    i have set up an initial homepage on my app.py page and i have begun adding visuals into the EDA.py page

---
+++ streamlit_app/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(page_title="Retail Sales Dashboard", layout="wide")
+
+st.title("🛍️ Retail Sales Analytics Dashboard")
+st.write("""
+Welcome to the Retail Sales Dashboard.  
+Using this app, you can explore product performance, pricing behaviour, as well as promotional impact. It also predicts sales volume using an XGBoost model.
+""")
+
---
+++ streamlit_app/pages/EDA.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+st.title("📊 Exploratory Data Analysis")
+
+st.write("### Price Distribution")
+st.image("../visuals/eda/price_distribution.png")
+
+st.write("### Sales Volume Distribution")
+st.image("../visuals/eda/sales_volume_distribution.png")
+
+st.write("### Product Category Distribution")
+st.image("../visuals/eda/product_category_distribution.png")
+
+st.write("### Sales Volume by Category")
+st.image("../visuals/eda/sales_volume_by_category.png")
+
+st.write("### Sales Volume by Position")
+st.image("../visuals/eda/sales_volume_by_position.png")
+
+st.write("### Promotion Effectiveness")
+st.image("../visuals/eda/promotion_effectiveness.png")
+
+st.write("### Promotion vs Sales Volume")
+st.image("../visuals/eda/promotion_vs_sales_volume.png")
+
+st.write("### Price vs Sales Volume")
+st.image("../visuals/eda/price_vs_sales_volume.png")
+
+st.write("### Correlation Heatmap")
+st.image("../visuals/eda/correlation_heatmap.png")
